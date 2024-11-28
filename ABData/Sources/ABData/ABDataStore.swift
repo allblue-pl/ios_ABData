0f76cb740ccbf85961128992d2188c343aad2129
AllBlue Monkey <j.zolcik@allblue.pl>
2024-11-28 16:15:52 +0100

    Development.

--- ABData/Sources/ABData/ABDataStore.swift
+++ ABData/Sources/ABData/ABDataStore.swift
@@ -4,22 +4,27 @@ import Foundation
 
 public class ABDataStore {
     
-    static public func getDBSchemeVersion(db: ABDatabase, transactionId: Int?, execute callback: @escaping (_ dbSchemeVer: Int?) -> Void) {
+    static public func getDBSchemeVersion(db: ABDatabase, transactionId: Int?, execute callback: @escaping (_ dbSchemeVer: Int) -> Void) {
         db.query_Select("SELECT Name, Data FROM _ABData_Settings WHERE Name = 'version'", [ SelectColumnType.String, SelectColumnType.JSON ]) { rows in
             guard rows.count > 0 else {
-                callback(nil)
+                callback(-1)
                 return
             }
             
-            guard let dbSchemeVersion = rows[0][1] as? Int else {
-                callback(nil)
+            guard let data = rows[0][1] as? [String:AnyObject] else {
+                callback(-1)
+                return
+            }
+            
+            guard let dbSchemeVersion = data["value"] as? Int else {
+                callback(-1)
                 return
             }
             
             callback(dbSchemeVersion)
         } execute: { error in
             print("ABDataStore Error -> \(error)")
-            callback(nil)
+            callback(-1)
         }
     }
     
